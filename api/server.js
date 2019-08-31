7e9f0a6c07cdc03300fbf6c87532a60baeeab613
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-30 21:35:40 -0400

    POST /api/auth/register

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -8,9 +9,10 @@ const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
 
-server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,50 @@
+const bcrypt = require('bcrypt');
 const router = require('express').Router();
+const users = require('./users');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  let user = req.body;
+  const { username, password } = user;
+  const length = Object.keys(user).length;
+
+  console.log(user);
+
+  try {
+    if (!length) {
+      res.status(400).json({
+        message: "No information.",
+        success: false
+      });
+    } else if (!username || !password) {
+      res.status(400).json({
+        message: "No username or password.",
+        success: false
+      });
+    } else {
+      user.password = await bcrypt.hash(password, 10);
+      
+      console.log('post-hash', user);
+
+      user = await users.new(user);
+
+      if (user) {
+        const newUser = await users.findByUsername(username);
+
+        console.log('newUser', newUser);
+
+        res.status(201).json({
+          message: "Head on in. You have registered successfully.",
+          success: true
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: "User could not be registered.",
+      success: false
+    });
+  }
 });
 
 router.post('/login', (req, res) => {
